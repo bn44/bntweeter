85e6c6aa2e3187627beacf4533d3fbaea504ecfd
bn44 <bn44@njit.edu>
2013-04-04 05:03:24 -0300

    Update stream.php

--- stream.php
+++ stream.php
@@ -58,7 +58,7 @@ require 'tmhOAuth.php';
 require 'tmhUtilities.php';
 $tmhOAuth = new tmhOAuth(array(
   'consumer_key'    => 'rnG4D4rtBJWaC5BM3AGUA',
-  'consumer_secret' => '00FMeCy14TDstZs8Bd9JEh6VCWkMokKOlorfM2WE',
+  'consumer_secret' => '00FMeCy14TDstZs8Bd9JEh6VCWknaifMokKOlorfM2WE',
   'user_token'      => '82681941-1jIaPzGCCaj1GZwOAaXUoC4FGIMHSLjCMSpHb94GU',
   'user_secret'     => 'wM0AaAbvbXOhyAWa1WqRPwXYbel6SHJMwh9EhSSiE',
 ));
