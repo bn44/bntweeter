325cff64105388f7a7aacd9289795c11c3b023a5
Bader <bn44@njit.edu>
2013-04-04 04:00:35 -0400

    to push the firt try boilerplate + tweeter API

---
+++ app_tokens.php
@@ -0,0 +1,11 @@
+<?php
+
+// OAuth tokens for single-user programming
+// After you create an app you MUST copy 
+// the tokens and paste them into the following lines
+$consumer_key = 'rnG4D4rtBJWaC5BM3AGUA';
+$consumer_secret = '00FMeCy14TDstZs8Bd9JEh6VCWkMokKOlorfM2WE';
+$user_token = '82681941-1jIaPzGCCaj1GZwOAaXUoC4FGIMHSLjCMSpHb94GU';
+$user_secret = 'wM0AaAbvbXOhyAWa1WqRPwXYbel6SHJMwh9EhSSiE';
+
+?>
