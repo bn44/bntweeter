170de8fd01dc36ce43e26c3eb68b8d63b30257f3
bn44 <bn44@njit.edu>
2013-04-04 05:04:24 -0300

    Update app_tokens.php

--- app_tokens.php
+++ app_tokens.php
@@ -4,7 +4,7 @@
 // After you create an app you MUST copy 
 // the tokens and paste them into the following lines
 $consumer_key = 'rnG4D4rtBJWaC5BM3AGUA';
-$consumer_secret = '00FMeCy14TDstZs8Bd9JEh6VCWkMokKOlorfM2WE';
+$consumer_secret = '00FMeCy14TDstZs8Bd9JEh6VCWnaifkMokKOlorfM2WE';
 $user_token = '82681941-1jIaPzGCCaj1GZwOAaXUoC4FGIMHSLjCMSpHb94GU';
 $user_secret = 'wM0AaAbvbXOhyAWa1WqRPwXYbel6SHJMwh9EhSSiE';
 
